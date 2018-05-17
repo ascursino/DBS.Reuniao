5ce7dcff781d82770b412225450eeab8a68748bd
Andréa Cursino <ascursino@gmail.com>
2018-05-17 19:28:37 +1200

    Add project files.

---
+++ DBS.Reuniao/Layouts/DBS.Reuniao/Pages/DBS.Reuniao.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DBS.Reuniao.aspx.cs" Inherits="DBS.Reuniao.Layouts.DBS.Reuniao.DBS" DynamicMasterPageFile="~masterurl/default.master" %>
+
+<asp:Content ID="PageHead" ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
+<script type="text/javascript">
+    function AbreLinkDocto(vTitulo) {
+        var vCaminho = "/Doctos_Reuniao/Forms/AllItems.aspx?RootFolder=%2FDoctos%5FReuniao%2F" + escapeProperly(vTitulo);
+        window.open(vCaminho, "_blank");
+    }
+
+</script>
+</asp:Content>
+
+<asp:Content ID="Main" ContentPlaceHolderID="PlaceHolderMain" runat="server">
+    <asp:Label runat="server" ID = "lblErro" ForeColor="Red"></asp:Label>
+
+    <p class="subtitulo_pagina">:: Reunião</p>
+    <p>&nbsp;</p>
+
+    <table width="100%" border="0" cellpadding="0" cellspacing="1" class="grid_tabela">
+      <tr class="grid_cabecalho">
+        <td width="20%">Início</td>
+        <td width="*">Assunto</td>
+        <td width="30%">Local</td>
+        <td width="15%">&nbsp;</td>
+      </tr>
+
+      <asp:Literal ID="ltrConteudo" runat="server" />
+
+      <!--
+      <tr class="grid_conteudo">
+        <td>99/99/9999 99:00</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td align="center">
+    	    <img src="images/img_forum.png" width="26" height="26" alt="Fórum" /> &nbsp;&nbsp;&nbsp;
+            <img src="img_doctos.png" width="26" height="26" alt="Documentação" />
+        </td>
+      </tr>
+      -->
+
+    </table>
+    <p>&nbsp;</p>
+    <p><img src="/images/img_adicionar.png" alt="" />&nbsp;<a class="ms-addnew" id="idHomePageNewItem" href="/_layouts/listform.aspx?PageType=8&amp;ListId={1C1B2F66-5F96-4976-8AAA-A26AF0A737E9}&amp;RootFolder=" onclick="javascript:NewItem2(event, &quot;/_layouts/listform.aspx?PageType=8&amp;ListId={1C1B2F66-5F96-4976-8AAA-A26AF0A737E9}&amp;RootFolder=&quot;);javascript:return false;" target="_self">Adicionar novo item</a></p>
+
+</asp:Content>
+
+
---
+++ DBS.Reuniao/Layouts/DBS.Reuniao/Pages/DBS.Reuniao.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Data;
+using System.Web;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.WebControls;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace DBS.Reuniao.Layouts.DBS.Reuniao
+{
+    public partial class DBS : LayoutsPageBase
+    {
+        SPWeb web = SPContext.Current.Site.RootWeb;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                MontaGridConteudo("");
+            }
+            catch (Exception ex)
+            {
+                lblErro.Text = ex.ToString();
+            }
+        }
+
+
+        public void MontaGridConteudo(string varParamBusca)
+        {
+            string varLinha = "";
+            string varLinkForum = "";
+            string varLinkDocto = "";
+
+            int varIDForum;
+            int varIDDocto;
+
+            //Acessando a lista
+            SPList lstLista = web.Lists["Reuniao"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+
+            oQuery.Query =  "<Where>" +
+                            "    <Geq>" +
+                            "        <FieldRef Name='In_x00ed_cio' />" +
+                            "        <Value IncludeTimeValue='TRUE' Type='DateTime'>" + DateTime.Today.ToString("yyyy-MM-dd") + "</Value>" +
+                            "    </Geq>" +
+                            "</Where>" +
+                            "<OrderBy>" +
+                            "   <FieldRef Name='In_x00ed_cio' Ascending='True' />" +
+                            "</OrderBy>";
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstLista.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                StringBuilder sbLinha = new StringBuilder();
+
+                varIDForum = BuscaIDForum(AjustaCampo(oListItem["Title"].ToString()));
+
+                if (varIDForum == 0)
+                {
+                    varLinkForum = "<img src='/images/img_forum_off.png' alt='Fórum' border='0'/>&nbsp;&nbsp;&nbsp;";
+                }
+                else
+                {
+                    varLinkForum = "<a href='/Lists/Forum_Reuniao/Flat.aspx?RootFolder=%2FLists%2FForum%5FReuniao%2F" + varIDForum + "%5F%2E000' target='blank'><img src='/images/img_forum_on.png' alt='Fórum' border='0'/></a>&nbsp;&nbsp;&nbsp;";
+                }
+
+                varIDDocto = BuscaIDDocto(AjustaCampo(oListItem["Title"].ToString()));
+
+                if (varIDDocto == 0)
+                {
+                    varLinkDocto = "<img src='/images/img_doctos_off.png' alt='Documentação' border='0'/>";
+                }
+                else
+                {
+                    varLinkDocto = "<a href=\"#\" onclick=\"AbreLinkDocto('" + AjustaCampo(oListItem["Title"].ToString()) + "');\"><img src='/images/img_doctos_on.png' alt='Documentação' border='0'/></a>";
+                }
+
+                //Monta linha da grid com os resultados
+                sbLinha.Append("<tr class='grid_conteudo'>");
+                sbLinha.Append("<td>&nbsp;" + AjustaCampo(oListItem["In_x00ed_cio"].ToString()) + "</td>");
+
+                //sbLinha.Append("<td>&nbsp;" + AjustaCampo(oListItem["Title"].ToString()) + "</td>");
+
+                sbLinha.Append("<td>&nbsp;<a href=\"#\" onclick=\"openDialog2('/_layouts/listform.aspx?PageType=4&ListId={1C1B2F66-5F96-4976-8AAA-A26AF0A737E9}&ID=" + AjustaCampo(oListItem["ID"].ToString()) + "',1050,'Detalhes da Reunião');\">" + AjustaCampo(oListItem["Title"].ToString()) + "</a></td>");
+
+            //openDialog2('/_layouts/listform.aspx?PageType=4&ListId={1C1B2F66-5F96-4976-8AAA-A26AF0A737E9}&ID=51',1050,'Reunião')
+                
+                sbLinha.Append("<td>&nbsp;" + AjustaCampo(oListItem["Local"].ToString()) + "</td>");
+                sbLinha.Append("<td align='center'>");
+                sbLinha.Append(varLinkForum);
+                sbLinha.Append(varLinkDocto);
+                sbLinha.Append("</td></tr>");
+
+                varLinha += sbLinha.ToString();
+            }
+
+            ltrConteudo.Text = varLinha.ToString();
+        }
+
+
+        public string AjustaCampo(string vCampo)
+        {
+            if (vCampo.IndexOf(";#") > 0)
+            {
+                vCampo = vCampo.Substring(vCampo.IndexOf("#") + 1);
+            }
+            return vCampo;
+        }
+
+
+        public int BuscaIDForum(string varParam)
+        {
+            int vIDForum = 0;
+
+            //Acessando a lista
+            SPList lstLista = web.Lists["Forum_Reuniao"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+
+            oQuery.Query = "<Where>" +
+                            "    <Eq>" +
+                            "        <FieldRef Name='Title' />" +
+                            "        <Value Type='Text'>" + varParam.ToString() +"</Value>" +
+                            "    </Eq>" +
+                            "</Where>";
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstLista.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                if (collListItems.Count > 0)
+                {
+                    vIDForum = int.Parse(AjustaCampo(oListItem["ID"].ToString()));
+                }
+            }
+
+            return vIDForum;
+        }
+
+
+        public int BuscaIDDocto(string varParam)
+        {
+            int vIDDocto = 0;
+
+            //Acessando a lista
+            SPList lstLista = web.Lists["Documentos de Reunião"];
+
+            //Construindo a Query CAML
+            SPQuery oQuery = new SPQuery();
+
+            oQuery.Query = "<Where>" +
+                            "    <Eq>" +
+                            "        <FieldRef Name='Title' />" +
+                            "        <Value Type='Text'>" + varParam.ToString() + "</Value>" +
+                            "    </Eq>" +
+                            "</Where>";
+
+            //Criando uma coleção de itens, utilizando a Query CAML
+            SPListItemCollection collListItems = lstLista.GetItems(oQuery);
+
+            //Recupera o item
+            foreach (SPListItem oListItem in collListItems)
+            {
+                if (collListItems.Count > 0)
+                {
+                    vIDDocto = int.Parse(AjustaCampo(oListItem["ID"].ToString()));
+                }
+            }
+
+            return vIDDocto;
+        }
+
+
+    }
+}
